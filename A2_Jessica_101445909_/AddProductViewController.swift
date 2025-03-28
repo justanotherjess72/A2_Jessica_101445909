b31fccdcacc0c1f4ba229693c2cdd73bea31a977
justanotherjess72 <101445909@georgebrown.ca>
2025-03-28 12:48:10 -0700

    Updates to Add Product View Controller

--- A2_Jessica_101445909_/AddProductViewController.swift
+++ A2_Jessica_101445909_/AddProductViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class AddProductViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var providerField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemPink.withAlphaComponent(0.05)
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        guard
+            let name = nameField.text, !name.isEmpty,
+            let desc = descriptionField.text, !desc.isEmpty,
+            let priceText = priceField.text, let price = Double(priceText),
+            let provider = providerField.text, !provider.isEmpty
+        else {
+            showAlert("All fields are required and price must be a number.")
+            return
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        ProductManager.shared.addProduct(
+            id: UUID().uuidString,
+            name: name,
+            description: desc,
+            price: price,
+            provider: provider
+        )
+
+        showAlert("Snack added!") {
+            self.clearFields()
+        }
     }
-    */
 
+    func showAlert(_ message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: "Info", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in completion?() })
+        present(alert, animated: true)
+    }
+
+    func clearFields() {
+        nameField.text = ""
+        descriptionField.text = ""
+        priceField.text = ""
+        providerField.text = ""
+    }
 }
+
